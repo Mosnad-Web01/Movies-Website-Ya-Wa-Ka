da0e47bdfe556a99fbce5151889b7634d75eab35
YasserEsam <yaaser10esam1010@gmail.com>
2024-09-20 12:15:10 +0300

    fix: fix the error in the footer

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,30 @@
-import React from "react"
-import { Card, CardBody, CardFooter, Image } from "@nextui-org/react"
-import Circularprogress from "./CircularProgress"
+import React from "react";
+import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+import Circularprogress from "./CircularProgress";
 
-const CardComponent = ({ image, loading, title, progress, date, customClass }) => {
+const CardComponent = ({
+  image,
+  loading,
+  title,
+  progress,
+  date,
+  customClass,
+  isActor = false, // New prop to determine if the card is for an actor
+}) => {
   return (
     <Card
       shadow="sm"
       isPressable
-      className= {` inline-flex m-2 relative overflow-hidden ${customClass || "w-48"} `}
+      className={`inline-flex m-2 relative overflow-hidden ${customClass || "w-48"}`}
     >
       <CardBody className="p-0 relative">
         {loading ? (
           <div className="w-full h-[140px] bg-gray-300 animate-pulse"></div>
         ) : (
           <>
-            <Circularprogress progress={progress} />
+            {!isActor && progress !== undefined && ( // Only show progress if not an actor and progress is provided
+              <Circularprogress progress={progress} />
+            )}
             <Image
               className="w-full object-cover h-[140px]"
               src={image}
@@ -25,12 +35,12 @@ const CardComponent = ({ image, loading, title, progress, date, customClass }) =
           </>
         )}
       </CardBody>
-      <CardFooter className="text-small flex flex-col items-center ">
+      <CardFooter className="text-small flex flex-col items-center">
         <b className="truncate w-full max-w-full whitespace-nowrap overflow-hidden">{title}</b>
-        <small>{date}</small>
+        {date && <small>{date}</small>} {/* Show date only if provided */}
       </CardFooter>
     </Card>
-  )
-}
+  );
+};
 
-export default CardComponent
+export default CardComponent;
